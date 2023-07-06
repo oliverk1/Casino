20989c8ed1b08a1b7520e3f5cb584a6ebbbefb20
Oliver King <116508612+oliverk1@users.noreply.github.com>
2023-07-06 15:51:27 +0100

    Add files via upload

---
+++ Roulette.py
@@ -0,0 +1,81 @@
+import random
+
+def getBet(balance):
+    while True:
+        bet = input("You have £"+str(round(balance,2))+". How much would you like to bet, with a minimum of £5? (Type 0 to exit) ")
+        try:
+            bet = round(float(bet),2)
+        except ValueError:
+            print("Please enter numbers only.")
+            continue
+        if bet == 0:
+            break
+        elif bet <= balance and bet >= 5:
+            break
+        else:
+            print("You can not bet that amount.")
+            continue
+    return bet
+
+def getChoice():
+    while True:
+        choice = input("What number/colour (0-36 or Red/Green/Black or Odd/Even) would you like to bet on? ")
+        choice = choice.lower()
+        ifNum = choice.isdigit()
+        if ifNum is True:
+            if 0 <= int(choice) <= 36:
+                break
+            else:
+                print("Please enter a valid number/colour.")
+                continue
+        if choice == "red" or choice == "green" or choice == "black" or choice == "odd" or choice == "even":
+            break
+        else:
+            print("Please enter a valid number/colour.")
+            continue
+    return choice
+
+def rouletteSpin():
+    spin = random.randint(0,36)
+    if spin == 0:
+        colour = "green"
+    elif spin%2:
+        colour = "red"
+    else:
+        colour = "black"
+    return spin, colour
+
+def getWinnings(spin, colour, choice, bet):
+    win = -bet
+    print(spin, colour)
+    ifNum = choice.isdigit()
+    if (colour == "black" and choice == "black") or (colour == "red" and choice == "red") or (colour == "odd" and choice == "odd") or (colour == "even" and choice == "even"):
+        win = bet
+        print("You win 1 to 1.")
+    elif colour == "green" and choice == "green":
+        win = bet * 35
+        print("You win 35 to 1.")
+    elif ifNum is True:
+        if int(spin) == int(choice):
+            win = bet *35
+            print("You win 35 to 1.")
+    return win
+
+def main():
+    balance = "£10.74"
+    balance = float(balance.lstrip("£"))
+    while round(balance, 2) > 0:
+        print("Balance: £"+str(round(balance,2)))
+        bet = getBet(balance)
+        if bet == 0:
+            break
+        choice = getChoice()
+        spin, colour = rouletteSpin()
+        win = getWinnings(spin, colour, choice, bet)
+        balance = balance + win
+        print("\n")
+
+    print("Thanks for playing!"
+          "\nTotal balance remaining: £"+str(round(balance,2)))
+
+main()
---
+++ RouletteSimulationAnalysis.py
@@ -0,0 +1,53 @@
+import random
+
+def rouletteSpin():
+    spin = random.randint(0,36)
+    if spin == 0:
+        colour = "green"
+    elif spin%2:
+        colour = "red"
+    else:
+        colour = "black"
+    return spin, colour
+
+def getWinnings(spin, colour, choice, bet):
+    win = -bet
+    ifNum = choice.isdigit()
+    if (colour == "black" and choice == "black") or (colour == "red" and choice == "red") or (colour == "odd" and choice == "odd") or (colour == "even" and choice == "even"):
+        win = bet
+    elif colour == "green" and choice == "green":
+        win = bet * 35
+    elif ifNum is True:
+        if int(spin) == int(choice):
+            win = bet *35
+    return win
+
+def main():
+    total1 = 0
+    total35 = 0
+    loss = 0
+    win1to1 = []
+    win35to1 = []
+    bet = 5
+    repeat = 1000000
+    for i in range(repeat):
+        spin, colour = rouletteSpin()
+        win1to1.append(getWinnings(spin, colour, "red", bet))
+        win35to1.append(getWinnings(spin, colour, "7", bet))
+    for row in win1to1:
+        total1 = total1 + row
+        if row > 0:
+            loss = loss + row
+    for row in win35to1:
+        total35 = total35 + row
+        if row > 0:
+            loss = loss + row
+    totalProfit = ((repeat*bet)*2) - loss
+    total1 = total1 / repeat
+    total35 = total35 / repeat
+    print("Out of",repeat,"spins:"
+            "\nTotal profit on £"+str(round(repeat*2*bet,2)),"of bets is £"+str(round(totalProfit,2)),
+            "\nThe average return on £5 per bet 1 to 1 bets is",round(total1,2),"pounds."
+            "\nThe average return on £5 per bet on 35 to 1 bets is",round(total35,2),"pounds.")
+
+main()
