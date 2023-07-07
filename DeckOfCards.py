30f3728a0059785ce109f85dd4336c36b373ddcf
Oliver King <116508612+oliverk1@users.noreply.github.com>
2023-07-07 10:12:52 +0100

    Add files via upload

---
+++ DeckOfCards.py
@@ -0,0 +1,12 @@
+cards = []
+Suits = ['Club', 'Diamond',
+         'Heart', 'Spade']
+Ranks = ['A', '2', '3', '4',
+         '5', '6', '7', '8',
+         '9', '10', 'J', 'Q', 'K']
+  
+for rank in Ranks:
+  
+    for suit in Suits:
+        cards.append([rank,suit])
+print(cards)
